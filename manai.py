3396c751c366159d43c64fe817217c1a4a0cc551
Shawn Wilkinson <me@super3.org>
2020-07-19 12:32:18 -0400

    Basic App

---
+++ manai.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import openai
+from config import *
+openai.api_key = api_key
+
+prompt = """
+Q: list files in a directory
+> ls
+
+Q: delete a file named bar.txt
+> rm bar.txt
+
+Q: create a new directory named foo
+> mkdir foo
+
+Q: create a file named test.txt with the colors of the rainbow
+> echo "red orange yellow green blue indigo violet" > test.txt
+
+"""
+
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    usr_prompt = prompt + "Q: " + request.args.get("name", "World")
+    response = openai.Completion.create(engine="davinci", prompt=usr_prompt, temperature=0, stop="\n\n")
+    print(usr_prompt)
+    print(response.choices[0].text)
+    return response.choices[0].text
